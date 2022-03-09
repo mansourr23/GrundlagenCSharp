40f5a953700f0138fe48711b2e8c914bae0baf67
Mansour Alassaf <mansour.a3@gso.schule.koeln>
2022-03-09 11:57:49 +0100

    Arrays Aufgabe1

--- Arrays_ Noten/Program.cs
+++ Arrays_ Noten/Program.cs
@@ -6,7 +6,32 @@ namespace Arrays__Noten
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int Note = 0, schüler = 0;
+            double Notenschnitt = 0;
+            int[] noten = new int[schüler];
+            Console.WriteLine("Wie viele Schüler haben die Klassenarbeit geschrieben?");
+            schüler = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+
+
+            for (int i = 0; i < schüler; i++)
+            {
+                Console.WriteLine("Bitte Geben sie die Noten der schüler ein");
+
+
+
+                Note = Convert.ToInt32(Console.ReadLine());
+
+
+
+                Notenschnitt = Notenschnitt + Note;
+            }
+            Console.WriteLine("ihr durchschnitt lautet {0:F2}", Notenschnitt / schüler);
         }
     }
 }
+
+
+
+  
